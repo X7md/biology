729a963425fac588e031089e4045ea907fa65fac
x7md <it7mdx@googlemail.com>
2021-03-27 10:36:52 +0300

    FiSearch Bar

--- js/vueApp.js
+++ js/vueApp.js
@@ -81,10 +81,8 @@ var search_ = Vue.component('vx-search',{
     <div class="mt-4 col-10 col-lg-3">
     <div class="input-group mb-3">
 <input type="text" v-model="text" class="form-control text-light bg-dark" placeholder="ابحث في التحديات..." aria-label="Saerch" aria-describedby="button-addon2">
-<div class="input-group-append">
   <button class="btn btn-outline-dark bg-dark" type="button" v-on:click="do_()" id="button-search"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-image-fill"><path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"></path>
 </svg> بحث</button>
-</div>
 </div>
     </div>
     </div>`
