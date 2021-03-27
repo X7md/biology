d0281829c5322eb7e254fdf4479960a764eba747
x7md <it7mdx@googlemail.com>
2021-03-27 08:26:13 +0300

    push to github

---
+++ js/vueApp.js
@@ -0,0 +1,115 @@
+let mainPage = Vue.component('vx-main',{
+  template: `
+    <div>
+    <vx-search v-on:custom="customHandler"></vx-search>
+    <section class="row justify-content-center">
+            <div class="col-lg-4 col-md-6"  v-for="value in arr">
+               <router-link class="goto" :to="value.link"> <div class="card mt-2 bg-dark"> <div class="card-body d-inline-flex">
+              <div class="info-body"><p class="card-text font-weight-light"v-text="value.title"></p></div><div data-image><img :src="value.imageSrc" v-on:error="ImageError" class="img-fluid" alt="Responsive image"></div>
+          </div>
+            <div class="card-footer text-muted">
+            <div>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-book" viewBox="0 0 16 16">
+            <path d="M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811V2.828zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492V2.687zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z"/>
+          </svg><span class="iconFooter" v-text="value.pageNambuer"></span>
+          </div>
+            </div>
+                </div>
+                   </router-link>
+            </div>
+            <nav v-if="arr.length >= 9" aria-label="Page navigation">
+            <ul class="pagination justify-content-center">
+              <li class="page-item">
+                <a class="page-link" href="#">السابق</a>
+              </li>
+              <li class="page-item"><a class="page-link" href="#">1</a></li>
+              <li class="page-item"><a class="page-link" href="#">2</a></li>
+              <li class="page-item"><a class="page-link" href="#">3</a></li>
+              <li class="page-item">
+                <a class="page-link" href="#" tabindex="-1">التالي</a>
+              </li>
+            </ul>
+          </nav>
+</section>
+</div>`,
+    data: function () {
+        return {
+          arr: [
+            {"title": "نسبة المياه العذبة على كوكب الأرض",
+              "pageNambuer": "علم بيئة، الصفحة 2",
+              "imageSrc":"https://wallpaperaccess.com/full/3971906.jpg",
+              "link": "/"
+            },
+            {"title": "نسبة المياه العذبة على كوكب الأرض",
+              "pageNambuer": "علم بيئة، الصفحة 2",
+              "imageSrc":"",
+              "link": "/"
+            }
+          ],
+          data_: null
+        }
+      },
+      methods: {
+        customHandler : function(e){
+          this.data_ = e;
+        },
+        ImageError: function(e){
+          e.target.src = "static-files/image-404.png"
+        }
+      },
+      updated: function () {
+        this.$nextTick(function () {
+          var alertNode = this.$refs["alert"]
+          var alert = bootstrap.Alert.getInstance(alertNode)
+          this.$refs[".btn-close"].addEventListener("click", e => {alert.close()})
+        })
+      }
+    })
+
+var search_ = Vue.component('vx-search',{
+  data() {
+    return {
+      text: null
+    }
+  },
+  methods: {
+    do_: function (e) {
+      this.$emit('custom', this.text);
+    }
+  },
+    template: `<div class="row justify-content-center">
+    <div class="mt-4 col-10 col-lg-3">
+    <div class="input-group mb-3">
+<input type="text" v-model="text" class="form-control text-light bg-dark" placeholder="ابحث في التحديات..." aria-label="Saerch" aria-describedby="button-addon2">
+<div class="input-group-append">
+  <button class="btn btn-outline-dark bg-dark" type="button" v-on:click="do_()" id="button-search"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-image-fill"><path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"></path>
+</svg> بحث</button>
+</div>
+</div>
+    </div>
+    </div>`
+})
+
+const routes = [
+    {path: '/', component: mainPage}
+]
+
+const router = new VueRouter({
+    routes,
+    linkActiveClass: "active",
+    linkExactActiveClass: "exact-active"
+})
+
+var app = new Vue({
+    router,
+    methods: {
+      getCookie: function (name) {
+        const value = `; ${document.cookie}`;
+        const parts = value.split(`; ${name}=`);
+        if (parts.length === 2) return parts.pop().split(';').shift();
+      },
+      setCookie: function(name, value){
+        document.cookie = name + '=' + value + ';';
+      }
+    }
+}).$mount('#app')
